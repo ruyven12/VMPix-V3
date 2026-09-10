db20e80c9eeddddfb21a5d22837830e301ed7d2d
ruyven12 <ruyven12@gmail.com>
2026-09-10 12:11:52 -0400

    A

--- modules/wrestling.js
+++ modules/wrestling.js
@@ -18476,7 +18476,7 @@ function ensureHallCrusadesHaze(targetShell = wrestlingShowsShell) {
   };
 
   const isMobileCinderViewport = window.matchMedia?.("(max-width: 540px)")?.matches ?? window.innerWidth <= 540;
-  const distantCinderCount = isMobileCinderViewport ? 30 : 250;
+  const distantCinderCount = isMobileCinderViewport ? 15 : 250;
   const foregroundCinderCount = isMobileCinderViewport ? 4 : 15;
   const fragment = document.createDocumentFragment();
   const distantLayer = findHallCrusadesAmbientNode('[data-hall-crusades-cinders="distant"]') || createLayer("distant", distantCinderCount);
