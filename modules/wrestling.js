a67703db2951c6d3fe3b0d48e76f709024507193
ruyven12 <ruyven12@gmail.com>
2026-09-10 07:59:54 -0400

    Update wrestling.js

--- modules/wrestling.js
+++ modules/wrestling.js
@@ -18380,7 +18380,7 @@ function ensureHallCrusadesHaze(targetShell = wrestlingShowsShell) {
 
   const isMobileCinderViewport = window.matchMedia?.("(max-width: 540px)")?.matches ?? window.innerWidth <= 540;
   const distantCinderCount = isMobileCinderViewport ? 70 : 250;
-  const foregroundCinderCount = isMobileCinderViewport ? 8 : 15;
+  const foregroundCinderCount = isMobileCinderViewport ? 4 : 15;
   const fragment = document.createDocumentFragment();
   const distantLayer = findHallCrusadesAmbientNode('[data-hall-crusades-cinders="distant"]') || createLayer("distant", distantCinderCount);
   const foregroundLayer = findHallCrusadesAmbientNode('[data-hall-crusades-cinders="foreground"]') || createLayer("foreground", foregroundCinderCount);
